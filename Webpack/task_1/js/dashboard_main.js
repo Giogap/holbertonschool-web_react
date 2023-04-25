2ad11b6839101a7aea4c64b0a2e81998625bc22b
Giogap <giogap@gmail.com>
2023-04-25 10:47:04 -0500

    upload 1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,7 @@
+$(function() {
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<button>Click here to get started</button>');
+	$('body').append("<p id='count'></p>");
+	$('body').append('<p>Copyright - Holberton School</p>');
+});
