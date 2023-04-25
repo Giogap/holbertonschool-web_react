b714850eac028718739eb0c4026de836ee54d4e1
Giogap <giogap@gmail.com>
2023-04-25 13:40:29 -0500

    upload 2

---
+++ Webpack/task_2/README.md
@@ -0,0 +1,9 @@
+# Webpack
+
+## 1. Learning how to use Webpack with a config file
+
+- Create a folder named task_1, cd into it, and create a package.json using npm init -y
+
+npm install webpack webpack-cli --save-dev
+npm install jquery --save
+npm install lodash --save
---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+let count = 0;
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+	count += 1;
+	return count;
+}
+
+let debouncedFunc = _.debounce(() => {
+    let count = updateCounter();
+    $('#count').text(`${count} clicks on the button`);
+});
+$('button').on('click', debouncedFunc);
+
+_.debounce(updateCounter, 500);
+updateCounter();
