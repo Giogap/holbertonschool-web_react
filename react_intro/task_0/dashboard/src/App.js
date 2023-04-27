f6773903daae9efd25c9878050605f3add4b4d8f
Giogap <giogap@gmail.com>
2023-04-27 14:28:29 -0500

    upload 0

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import logo from './holberton_logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">        
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
