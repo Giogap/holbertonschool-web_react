fe346156cc215a8a54b845f75b59399d284fd9fd
Giogap <giogap@gmail.com>
2023-04-27 08:32:01 -0500

    upload 2

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
 		path: path.resolve(__dirname, 'public'),
 		filename: 'bundle.js',
 	},
+
+	module: {
+	  rules: [
+		{
+		  test: /\.css$/i,
+		  use: ['style-loader', 'css-loader']
+		},
+		{
+		  test: /\.(png|svg|jpg|jpeg|gif)$/i,
+		  type: 'asset/resource',
+		}
+	  ]
+	},
 	mode: 'production',
 };
+
