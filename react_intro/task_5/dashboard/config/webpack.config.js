9c7c6cd78bc73da41e0deb5db9cae001b1106bd2
Giogap <giogap@gmail.com>
2023-05-09 12:28:23 -0500

    upload 5

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
