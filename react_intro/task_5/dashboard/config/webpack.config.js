4172e66d740f01b6bdaf70f2f524acf675776def
Giogap <giogap@gmail.com>
2023-05-16 13:59:24 -0500

    upload

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -3,23 +3,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+	devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
-      },
-    ],
-  },
+	devServer: {
+		hot: true,
+		contentBase: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './dist/index.html',
+      title: 'Holberton Dashboard',
+    }),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+				loader: 'image-webpack-loader',
+			},
+		],
+	},
 };
