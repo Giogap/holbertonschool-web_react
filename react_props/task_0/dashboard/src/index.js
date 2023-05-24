e1949bd819444082cf1a5d1fe25fbfeb2fe3f475
Giogap <giogap@gmail.com>
2023-05-23 21:57:16 -0500

    upload

--- react_props/task_0/dashboard/src/index.js
+++ react_props/task_0/dashboard/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Notifications from './Notifications';
+import App from './App/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div id='root-notifications'>
-      <Notifications />
-    </div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
