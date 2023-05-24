77b347158025e9c72b022da2ab0af7aa3ffbcf44
Giogap <giogap@gmail.com>
2023-05-23 23:39:48 -0500

    upload 2

--- react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Notifications from './Notifications';
 
-const wrapper = shallow(<Notifications />);
+describe('<Notification />', () => {
+  const wrapper = shallow(<Notifications />);
+  it('renders without crashing', () => {
+    shallow(<Notifications />);
+  });
 
-it('renders without crashing', () => {
-  shallow(<Notifications />);
-});
-it('renders three list items', () => {
-  expect(wrapper.find('li').children().length).toEqual(3);
-});
-it('renders the <p>', () => {
-  expect(
-    wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)
-  ).toBeTruthy();
+  it('renders NotificationItem', () => {
+    const nItem = wrapper.find('NotificationItem');
+    expect(nItem).toBeDefined();
+    expect(nItem.first().html()).toEqual(
+      '<li data-notification-type="default">New course available</li>'
+    );
+  });
+
+  it('renders the <p>', () => {
+    expect(
+      wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)
+    ).toBeTruthy();
+  });
 });
