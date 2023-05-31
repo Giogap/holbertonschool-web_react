432f076b6ef8ad211a2fcd2f8b570ec3202a515d
Giogap <giogap@gmail.com>
2023-05-31 13:06:50 -0500

    upload 1

--- react_state/task_1/dashboard/src/Login/Login.js
+++ react_state/task_1/dashboard/src/Login/Login.js
@@ -1,23 +1,99 @@
-function Login() {
-  return (
-    <main role='main' className={css(styles.login)}>
-      <p>Login to access the full dashboard</p>
-      <label htmlFor='email'>Email:</label>
-      <input className={css(styles.inp)} type='email' name='email' id='email' />
-      <label htmlFor='password'>Password:</label>
-      <input
-        className={css(styles.inp)}
-        type='password'
-        name='password'
-        id='password'
-      />
-      <button className={css(styles.btn)} type='button'>
-        OK
-      </button>
-    </main>
-  );
-}
+import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: props.isLoggedIn,
+      email: '',
+      password: '',
+      enableSubmit: false,
+    };
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  handleLoginSubmit(e) {
+    e.preventDefault();
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleChangeEmail(e) {
+    const { value } = e.target;
+    const { password } = this.state;
+
+    if (value !== '' && password !== '') this.setState({ enableSubmit: true });
+    else this.setState({ enableSubmit: false });
+
+    this.setState({ email: e.target.value });
+  }
+
+  handleChangePassword(e) {
+    const { value } = e.target;
+    const { email } = this.state;
+
+    if (email !== '' && value !== '') this.setState({ enableSubmit: true });
+    else this.setState({ enableSubmit: false });
+
+    this.setState({ password: e.target.value });
+  }
+
+  render() {
+    return (
+      <main role='main' className={css(styles.login)}>
+        <p>Login to access the full dashboard</p>
+        <form action='' onSubmit={this.handleLoginSubmit}>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className={css(styles.inp)}
+            value={this.state.email}
+            onChange={this.handleChangeEmail}
+          />
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            className={css(styles.inp)}
+            value={this.state.password}
+            onChange={this.handleChangePassword}
+          />
+          <button
+            type='submit'
+            className={css(styles.btn)}
+            disabled={!this.state.enableSubmit}
+          >
+            OK
+          </button>
+        </form>
+      </main>
+    );
+  }
+}
+/*
+<label htmlFor='email'>Email:</label>
+        <input
+          className={css(styles.inp)}
+          type='email'
+          name='email'
+          id='email'
+        />
+        <label htmlFor='password'>Password:</label>
+        <input
+          className={css(styles.inp)}
+          type='password'
+          name='password'
+          id='password'
+        />
+        <button className={css(styles.btn)} type='button'>
+          OK
+        </button>
+*/
 const screenSize = {
   small: '@media screen and (max-width: 900px)',
 };
