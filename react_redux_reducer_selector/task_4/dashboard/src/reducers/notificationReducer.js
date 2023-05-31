e44c74fcfaa51db9ef0bc3bb4c2d85f9358811aa
Giogap <giogap@gmail.com>
2023-05-31 16:17:14 -0500

    upload 4

--- react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,39 +1,40 @@
+import { Map } from 'immutable';
 import {
-    FETCH_NOTIFICATIONS_SUCCESS,
-    MARK_AS_READ,
-    SET_TYPE_FILTER,
-  } from '../actions/notificationActionTypes';
-  
-  export const initialNotificationState = {
-    notifications: [],
-    filter: 'DEFAULT',
-  };
-  
-  const notificationReducer = (state = initialNotificationState, action) => {
-    switch (action.type) {  
-      case MARK_AS_READ:
-        return {
-          ...state,
-          notifications: state.notifications.map((notification) => {
-            const current = {
-              ...notification,
-            };
-            if (notification.id == action.index) current.isRead = true;
-  
-            return current;
-          }),
-        };
-  
-      case SET_TYPE_FILTER:
-        return {
-          ...state,
-          filter: action.filter,
-        };
-  
-      default:
-        break;
-    }
-    return state;
-  };
-  
-  export default notificationReducer;
+  FETCH_NOTIFICATIONS_SUCCESS,
+  MARK_AS_READ,
+  SET_TYPE_FILTER,
+} from '../actions/notificationActionTypes';
+
+export const initialNotificationState = {
+  notifications: [],
+  filter: 'DEFAULT',
+};
+
+import notificationsNormalizer from '../schema/notifications';
+
+const notificationReducer = (state = Map(initialNotificationState), action) => {
+  switch (action.type) {
+    case FETCH_NOTIFICATIONS_SUCCESS:
+      const normalizedData = notificationsNormalizer(action.data);
+
+      Object.keys(normalizedData.notifications).map((key) => {
+        normalizedData.notifications[key].isRead = false;
+      });
+      return state.merge(normalizedData);
+
+    case MARK_AS_READ:
+      return state.setIn(
+        ['notifications', String(action.index), 'isRead'],
+        true
+      );
+
+    case SET_TYPE_FILTER:
+      return state.set('filter', action.filter);
+
+    default:
+      break;
+  }
+  return state;
+};
+
+export default notificationReducer;
